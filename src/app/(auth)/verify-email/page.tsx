0c9a9e26b7740e53e657d2a5cdf95a482271bb3b
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2024-08-30 17:03:25 +0100

    just another commit

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,5 +1,10 @@
 const VerifyEmailPage = () => {
+  return (
+    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350]"></div>
 
+    </div>
+  )
 }
 
 export default VerifyEmailPage;
