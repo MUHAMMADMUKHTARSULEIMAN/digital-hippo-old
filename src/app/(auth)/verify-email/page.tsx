33ac024f41cef8ab4eca3659a616466abb617b9a
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2024-08-30 17:35:42 +0100

    just another commit

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface VEPProps {
   searchParams: {
     [key: string]: string | string[] | undefined
@@ -5,6 +7,8 @@ interface VEPProps {
 }
 const VerifyEmailPage = ({searchParams}: VEPProps) => {
   const token = searchParams.token;
+  const toEmail = searchParams.to;
+
   return (
     <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350]">
@@ -14,7 +18,15 @@ const VerifyEmailPage = ({searchParams}: VEPProps) => {
           </div>
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
-
+            <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+              <Image src={"hippo-email-sent.png"} alt={"Digital Hippo email sent image"} fill/>
+            </div>
+            <h3 className="font-semibold text-2xl">Check your email</h3>
+            {toEmail ? (
+              <p className="text-muted-foreground text-center">
+                We&apos;ve sent a verification link to <span>{toEmail}</span>
+              </p>
+            ) : null}
           </div>
         )}
       </div>
