c79238e36beadde8f350c0b6ea36cc7b891d24d0
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2024-09-18 06:19:46 +0100

    just another commit

---
+++ dist/config/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        value: "ui_kits",
+        label: "UI Kits",
+        featured: [
+            {
+                name: "Editor's Picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg",
+            },
+            {
+                name: "Bestsellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ],
+    },
+    {
+        value: "icons",
+        label: "Icons",
+        featured: [
+            {
+                name: "Favourite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg",
+            },
+            {
+                name: "Bestsellers",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg",
+            },
+        ],
+    },
+];
